fa7cb16e70a478ebda01cbe99a745fb9b7caedbe
cristi <cristi@cristi-laptop.(none)>
2009-11-04 10:45:39 +0200

    fix current project for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ActiveRecord::Base
   belongs_to :current_project, :class_name => "Project", :foreign_key => "current_project_id"
   
   def collaboratores
+    if current_project.nil?
+      current_project = projects.first
+      self.save
+    end
     current_project.users.collect{ |u| [u.name, u.id]}
   end
   
